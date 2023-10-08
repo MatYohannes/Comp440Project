30b3b4f500601940e917dbad3089cf17b8608b5b
TwishiS <twishi.saran@gmail.com>
2023-10-08 11:29:34 -0700

    pushing stored procedure to check all user details and validate user email password and username

--- database-sql-scripts/get_user_Details.sql
+++ database-sql-scripts/get_user_Details.sql
@@ -1,4 +1,29 @@
-USE comp440_databse_project;
+/* 
+	Project Name: COMP 440 Database Project - Group 5
+    Script Details: This is the script will create a procedure that is called in UI to
+					get all the current user details in the database of our project
+    Created By: Twishi Saran
+    Updated On: 2023-10-07 17:22:22
+    
+    execution statement example: 
+			call get_alluserDetails ();
 
-select * from comp440_databse_project.userDetails;
+*/
+
+DELIMITER $$
+USE comp440_databse_project$$
+DROP PROCEDURE IF EXISTS get_all_userDetails$$
+
+CREATE PROCEDURE get_alluserDetails ()
+BEGIN
+
+         SELECT userName as UserName,  
+				CONCAT(user_firstName,' ',user_lastName) AS FullName,
+                user_emailID as EmailID
+		   FROM comp440_databse_project.userDetails
+		ORDER BY FullName;
+
+END$$
+DELIMITER ;
+
 
---
+++ database-sql-scripts/validate_user_Details.sql
@@ -0,0 +1,62 @@
+/* 
+	Project Name: COMP 440 Database Project - Group 5
+    Script Details: This is the script will create a procedure that is called in UI to
+					get validate the user details in the database of our project
+    Created By: Twishi Saran
+    Updated On: 2023-10-07 17:22:22
+    
+    execution statement example: 
+			CALL check_userDetails("userName","userPassword","userEmail",  @ValidationResult);
+			SELECT @ValidationResult; 
+
+*/
+
+DELIMITER $$
+USE comp440_databse_project$$
+DROP PROCEDURE IF EXISTS check_userDetails$$
+
+CREATE PROCEDURE check_userDetails (IN user_Name VARCHAR(30), 
+									IN user_Password VARCHAR(25),
+									IN user_EmailID NVARCHAR(320),
+                                    OUT ValidationResult VARCHAR(255)) 
+BEGIN
+	  
+    DECLARE var_userName varchar(30); 
+    DECLARE var_userPswd  varchar(25); 
+    DECLARE var_userEmail varchar(320);
+    DECLARE userCount varchar(10);
+	 
+	SET var_userName = user_Name;
+    SET var_userPswd = user_Password;
+    SET var_userEmail = user_EmailID; 
+   
+
+    -- Check if any of the input parameters are null
+    IF var_userName IS NULL OR var_userEmail IS NULL OR var_userPswd IS NULL THEN
+        SET ValidationResult = 'Invalid Input';
+        
+    ELSE
+        -- Check if the combination exists in the database
+        SELECT IF(COUNT(distinct userName)  > 0, 'true', 'false') INTO userCount
+         FROM comp440_databse_project.userDetails
+        WHERE (userName = var_userName OR user_emailID = var_userEmail)
+          AND userPassword = var_userPswd;
+          
+            -- Set the result based on the userCount
+        IF userCount = 'true' THEN
+            SET ValidationResult = 'User Exists';
+        ELSE
+            SET ValidationResult = 'User Does Not Exist';
+        END IF;
+          
+    END IF;
+    
+    SELECT ValidationResult;
+    
+END$$
+DELIMITER ;
+ 
+
+
+
+ 
