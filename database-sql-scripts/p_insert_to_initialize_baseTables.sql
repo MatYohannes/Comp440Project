4d349188e7e3e1da7e809dc6522aa90a78c980b6
TwishiS <twishi.saran@gmail.com>
2023-11-05 01:39:45 -0700

    updated sql phase 2 proc for initializing data

--- database-sql-scripts/p_insert_to_initialize_baseTables.sql
+++ database-sql-scripts/p_insert_to_initialize_baseTables.sql
@@ -42,8 +42,7 @@ BEGIN
 		CONSTRAINT chk_userReview CHECK (userReview IN ('Excellent', 'Good', 'Fair', 'Poor')),
         PRIMARY KEY (reviewID)
 		);
-	 
-
+	  
         
 	INSERT INTO `comp440_database_project`.`itemDetails`(`itemTitle`,
 														`itemDescription`,
@@ -51,32 +50,37 @@ BEGIN
 														`itemPrice`,
 														`userName`,
 														`DateofListing`)
-			VALUES( ('SAMSUNG Galaxy Z Fold 5 Cell Phone',
+			VALUES 
+            ('SAMSUNG Galaxy Z Fold 5 Cell Phone',
 					'Android Smartphone 512GB with Big 7.6 Screen for Streaming, Gaming. Icy Blue.',
                     'electronics, cell phone, smartphone', 
-					 218.20,    'adminUser',    CURDATE()),
-                      ('BANGSON Small Refridgerator',
+					 218.20,    'adminUser',  CURDATE()),
+			('BANGSON Small Refridgerator',
   					'Small Refrigerator with Freezer, Silver.',
                       'electronics, home appliances, refridgerator', 279.99, 'adminUser',CURDATE()),
-                      ('Patio Heater for Outdoor Seating',
+			('Patio Heater for Outdoor Seating',
   					'Round Table Design, Double-Layer Stainless Steel Burner and Wheels, Outdoor Patio Heater',
                       'lawn and garden devices, outdoor cooling and heating, patio heater', 149.99, 'adminUser', CURDATE()),
-                      ('Portable Thermal Printer',
+			('Portable Thermal Printer',
   					'Bluetooth Mini Pocket Printer Compatible with Android & iOS.',
                       'office supplies, office electronics, receipt printers', 49.99, 'adminUser',CURDATE()),
-                      ('Fitbit Charge 6 Smartwatch',
+			('Fitbit Charge 6 Smartwatch',
   					'Fitness Tracker with Google apps, Heart Rate on Exercise Equipment,Black, One Size.',
                       'electronics, wearable devices, smartwatch', 159.95, 'adminUser',CURDATE())
-                  );
+			;
                   
                   
-  	INSERT INTO userReviews
-  		 	VALUES( (1,'Excellent','I love this phone. You can multitask in so many ways. GREAT BUY!','adminUser',CURDATE()),
-  					 (2,'Good','Great for students and easy to install. Works quietly.','adminUser',CURDATE()),
+  	INSERT INTO `comp440_database_project`.`userReviews` (`itemID`,
+											`userReview`,
+											`userReviewDescription`,
+											`userName`,
+											`DateofReview`)
+  		 	VALUES 	 (1,'Excellent','I love this phone. You can multitask in so many ways. GREAT BUY!','adminUser',CURDATE()),
+  					  (2,'Good','Great for students and easy to install. Works quietly.','adminUser',CURDATE()),
                       (3,'Poor','Noisy and Difficult to assemble','adminUser',CURDATE()),
                       (4,'Fair','Easy to use and compatible with phone but ink dries up soon.','adminUser',CURDATE()),
                       (5,'Excellent','Great UI super easy to use and understand.','adminUser',CURDATE())            
-                  );
+                  ;
 
 
 END$$
