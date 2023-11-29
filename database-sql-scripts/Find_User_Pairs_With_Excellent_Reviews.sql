326db8d9203daa565e37c8d595e5a2dc94470a6a
TwishiS <twishi.saran@gmail.com>
2023-11-29 11:57:13 -0800

    updated the sql procs for question 2

--- database-sql-scripts/Find_User_Pairs_With_Excellent_Reviews.sql
+++ database-sql-scripts/Find_User_Pairs_With_Excellent_Reviews.sql
@@ -17,17 +17,34 @@ DROP PROCEDURE IF EXISTS Find_User_Pairs_With_Excellent_Reviews$$
 
 CREATE PROCEDURE Find_User_Pairs_With_Excellent_Reviews()
 BEGIN
-    SELECT DISTINCT r1.reviewer AS UserA, r2.reviewer AS UserB
-    FROM reviews r1
-    JOIN reviews r2 ON r1.itemID = r2.itemID AND r1.reviewer != r2.reviewer
-    WHERE r1.reviewType = 'Excellent' AND r2.reviewType = 'Excellent'
-        AND NOT EXISTS (
-            SELECT 1
-            FROM reviews r3
-            WHERE r3.itemID = r1.itemID
-                AND r3.reviewer IN (r1.reviewer, r2.reviewer)
-                AND r3.reviewType != 'Excellent'
-        );
+		DROP TEMPORARY TABLE IF EXISTS user_pairs;
+        
+		CREATE TEMPORARY TABLE user_pairs AS
+				SELECT DISTINCT
+					i.userName AS UserA,
+					r.userName AS UserB
+				FROM itemDetails i
+				JOIN  userReviews r ON i.ItemID = r.ItemID
+				WHERE NOT EXISTS (
+						SELECT 1
+						FROM   userReviews r2
+						WHERE   r2.ItemID = i.ItemID 
+							AND r2.userReview <> 'Excellent'
+					)
+					AND NOT EXISTS (
+						SELECT 1
+						FROM userReviews r3
+						WHERE r3.ItemID = i.ItemID 
+							AND r3.userReview <> 'Excellent'
+					);
+
+		SELECT DISTINCT
+			LEAST(UserA, UserB) AS User1,
+			GREATEST(UserA, UserB) AS User2
+		from user_pairs;
+        
+        
+        
 END $$
 
 DELIMITER ;
